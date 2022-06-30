d5af751d287bdc3e20c3751013ae78e9703c22be
Fortune <fortchimez@gmail.com>
2022-06-30 18:46:26 +0100

    trivia api

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -21,16 +21,13 @@ def paginate_questions(request, selection):
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
+    
+    #Set up CORS. Allow '*' for origins.
     setup_db(app)
     cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-    """
-    @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-    """
 
-    """
-    @TODO: Use the after_request decorator to set Access-Control-Allow
-    """
+    #The after_request decorator to set Access-Control-Allow
     @app.after_request
     def after_request(response):
         response.headers.add(
@@ -41,11 +38,8 @@ def create_app(test_config=None):
         )
         return response
 
-    """ 
-    @TODO:
-    Create an endpoint to handle GET requests
-    for all available categories.
-    """
+    
+    #This endpoint to handles GET requests for all available categories.
     @app.route("/categories")
     def get_categories():
         try:
@@ -80,17 +74,8 @@ def create_app(test_config=None):
 
 
     """
-    @TODO:
-    Create an endpoint to handle GET requests for questions,
-    including pagination (every 10 questions).
-    This endpoint should return a list of questions,
-    number of total questions, current category, categories.
-
-
-    TEST: At this point, when you start the application
-    you should see questions and categories generated,
-    ten questions per page and pagination at the bottom of the screen for three pages.
-    Clicking on the page numbers should update the questions.
+    This endpoint handles GET requests for questions, including pagination (every 10 questions). This endpoint should return a list of questions, number of total questions, current category, categories.
+    At this point, starting the application loads questions and categories generated, ten questions per page and pagination at the bottom of the screen for three pages. Clicking on the page numbers updates the questions.
     """
     @app.route("/questions")
     def get_questions():
@@ -133,11 +118,8 @@ def create_app(test_config=None):
         # )
 
     """
-    @TODO:
-    Create an endpoint to DELETE question using a question ID.
-
-    TEST: When you click the trash icon next to a question, the question will be removed.
-    This removal will persist in the database and when you refresh the page.
+    This endpoint to DELETEs a question using a question ID.
+    On click the trash icon next to a question, the question is removed. This removal will persist in the database and when you refresh the page.
     """
     @app.route("/questions/<int:question_id>", methods=["DELETE"])
     def delete_question(question_id):
@@ -165,14 +147,8 @@ def create_app(test_config=None):
             abort(422)
 
     """
-    @TODO:
-    Create an endpoint to POST a new question,
-    which will require the question and answer text,
-    category, and difficulty score.
-
-    TEST: When you submit a question on the "Add" tab,
-    the form will clear and the question will appear at the end of the last page
-    of the questions list in the "List" tab.
+    This endpoint to POSTs a new question, which requires the question and answer text, category, and difficulty score.
+    On submiting a question on the "Add" tab, the form will clear and the question will appear at the end of the last page of the questions list in the "List" tab.
     """
     @app.route("/questions", methods=["POST"])
     def create_question():
@@ -218,14 +194,8 @@ def create_app(test_config=None):
             abort(422)
 
     """
-    @TODO:
-    Create a POST endpoint to get questions based on a search term.
-    It should return any questions for whom the search term
-    is a substring of the question.
-
-    TEST: Search by any phrase. The questions list will update to include
-    only question that include that string within their question.
-    Try using the word "title" to start.
+    This Creates a POST endpoint to get questions based on a search term. It returns any questions for whom the search term is a substring of the question.
+    You can search by any phrase. The questions list updates and includes only question that include that string within their question. You can try using the word "title" to start.
     """
     @app.route("/questions/search", methods=["POST"])
     def search_questions():
@@ -249,12 +219,8 @@ def create_app(test_config=None):
         abort(404)
 
     """
-    @TODO:
-    Create a GET endpoint to get questions based on category.
-
-    TEST: In the "List" tab / main screen, clicking on one of the
-    categories in the left column will cause only questions of that
-    category to be shown.
+    This creates a GET endpoint to get questions based on category.
+    In the "List" tab / main screen, clicking on one of the categories in the left column causes only questions of that category to be shown.
     """
     @app.route("/categories/<int:category_id>/questions")
     def get_question_category(category_id):
@@ -285,15 +251,8 @@ def create_app(test_config=None):
             abort(400)
 
     """
-    @TODO:
-    Create a POST endpoint to get questions to play the quiz.
-    This endpoint should take category and previous question parameters
-    and return a random questions within the given category,
-    if provided, and that is not one of the previous questions.
-
-    TEST: In the "Play" tab, after a user selects "All" or a category,
-    one question at a time is displayed, the user is allowed to answer
-    and shown whether they were correct or not.
+    This creates a POST endpoint to get questions to play the quiz. This endpoint takes category and previous question parameters and returns a random questions within the given category, if provided, and that is not one of the previous questions.
+    In the "Play" tab, after a user selects "All" or a category, one question at a time is displayed, the user is allowed to answer and shown whether they were correct or not.
     """
     @app.route("/quizzes", methods=["POST"])
     def get_quiz():
@@ -335,8 +294,7 @@ def create_app(test_config=None):
         })
 
     """
-    @TODO:
-    Create error handlers for all expected errors
+    Here are the error handlers for all expected errors
     including 404 and 422.
     """
     @app.errorhandler(404)
